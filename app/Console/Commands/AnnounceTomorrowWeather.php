8e9847ab8103056c47cff26ca273e96e53256978
ppen.019 <ppengineer.019@gmail.com>
2019-07-15 09:00:15 +0000

    add tomorrows weather, japanese correction

--- app/Console/Commands/AnnounceTomorrowWeather.php
+++ app/Console/Commands/AnnounceTomorrowWeather.php
@@ -4,21 +4,21 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class AnnounceWeather extends Command
+class AnnounceTomorrowWeather extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'command:todays_weather';
+    protected $signature = 'command:tomorrows_weather';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Announce todays weather';
+    protected $description = 'Announce tomorrows weather';
 
     /**
      * Create a new command instance.
@@ -38,24 +38,25 @@ class AnnounceWeather extends Command
     public function handle()
     {
         $app_id = env('OPEN_WEATHER_MAP_API_KEY', false);
-        $url = 'http://api.openweathermap.org/data/2.5/forecast?id=1850147&cnt=5&lang=ja&units=metric&APPID=' . $app_id;
+        $url = 'http://api.openweathermap.org/data/2.5/forecast?id=1850147&cnt=11&lang=ja&units=metric&APPID=' . $app_id;
         $response_json = file_get_contents($url); //外部サイトにあるjsonなどを取得する
         $response = json_decode($response_json, true);  // JSONデータを配列にする
     
         $weather =array();
         $temp = array();
-    
+        
         foreach($response['list'] as $weather_at_time){
             $weather[] = $weather_at_time['weather'][0]['description'];
             $temp[] = (int)$weather_at_time['main']['temp'];
         }
         
-        $message = '今日の天気' . PHP_EOL . '朝：' . $weather[0] . ', ' . $temp[0] . '℃' . PHP_EOL . '昼：' . $weather[2] . ', '. $temp[2] . '℃'. PHP_EOL . '夜：' . $weather[4] . ', '.$temp[4] . '℃';
+        $message = '明日の天気' . PHP_EOL . '朝：' . $weather[6] . ', ' . $temp[6] . '℃' . PHP_EOL . '昼：' . $weather[8] . ', '. $temp[8] . '℃'. PHP_EOL . '夜：' . $weather[10] . ', '.$temp[10] . '℃';
         
         $unnatural = array('厚い雲', '適度な雨');
         $natural = array('くもり', '雨');
         
         $message = str_replace($unnatural, $natural, $message);
+
         
         $authorization = "Authorization: Bearer " . env('LINE_ACCESS_TOKEN');
         
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\AnnounceWeather::class
+        Commands\AnnounceWeather::class,
+        Commands\AnnounceTomorrowWeather::class,
     ];
 
     /**
